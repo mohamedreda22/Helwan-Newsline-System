43da1001eeacd3090fc3848a85c4c5a6e166a3a9
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2024-02-14 03:41:03 +0200

    loading and warming up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import HomePage from './components/homePage';
+import Loading from './components/loading'; 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <Loading /> 
+      ) : (
+        <HomePage />
+      )}
     </div>
   );
 }
