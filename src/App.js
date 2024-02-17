dc25b7c3f8538a313c6302a07417aab839bf9253
Hager Amr <118662525+hager13203@users.noreply.github.com>
2024-02-17 21:25:45 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 ///////gdfdfdfsadfsdfsdfsd
+//hhhhhuguuuu
 
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import HomePage from './components/homePage';
-import Loading from './components/loading'; 
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import HomePage from "./components/homePage";
+import Loading from "./components/loading";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -11,20 +12,12 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000); 
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <div className="App">
-      {isLoading ? (
-        <Loading /> 
-      ) : (
-        <HomePage />
-      )}
-    </div>
-  );
+  return <div className="App">{isLoading ? <Loading /> : <HomePage />}</div>;
 }
 
 export default App;
