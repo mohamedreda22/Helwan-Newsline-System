270b48a936a39f8248680435578cf3aa8b96b13b
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2024-02-19 23:05:27 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-///////gdfdfdfsadfsdfsdfsd
-//hhhhhuguuuu
-//jjjjjjj
-//jjjpppp
-//Yasmina
-
-import React, { useState, useEffect } from "react";
->>>>>>> main
 import "./App.css";
 //import HomePage from "./components/homePage";
 //import Loading from "./components/loading";
