4814a24a7786f69cc5d140a7ade2a52a1adaec69
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2024-02-19 22:45:10 +0200

    fix package.json error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import {router} from './routes/Routes'
@@ -10,23 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <RouterProvider router={router}/>
 
-=======
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import SideBar from './components/SideBar';
- 
- import 'bootstrap/dist/css/bootstrap.min.css'
-//import AddArticle from './pages/AddArticle';
- 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <SideBar/>
-    {/* <AddArticle/> */}
-  </React.StrictMode>
->>>>>>> main
 );
 
 // If you want to start measuring performance in your app, pass a function
