3c612b3d111e73c7133fccf78d611c40753ae4f4
YasmeenAliNasser <ya607820@gamil.com>
2024-02-27 17:47:59 +0200

    Edit Article2

--- src/pages/EditArticle.js
+++ src/pages/EditArticle.js
@@ -1,80 +1,103 @@
-// import React from 'react';
-import React, { Component , useCallback } from 'react'
+ 
+import React, { useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import Button from "react-bootstrap/Button";
+// import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import "../styles/EditArticle.css"
+const EditArticle = () => {
 
-import '../styles/EditArticle.css'
-
-class EditArticle extends Component {
-
-    handleClick =()=>{
-           console.log(this.state.Name)
-         }
-
-         change =()=>{
-            console.log(this.state.Age)
-          }
-    state = { 
-        Name:'Child',
-        Age:22,
-     } 
-
-
-    //  const onDrop = useCallback(acceptedFiles => {
-    //     // هنا يمكنك التعامل مع الملفات المقبولة (acceptedFiles)
-    //     // مثلاً، يمكنك إجراء إرسال الصورة إلى الخادم أو عرضها مباشرة في التطبيق
-    //     console.log(acceptedFiles);
-    //   }, []);
+    // const [file, setFile] = useState(null);
+      
+    // const handleFileChange = (e) => {
+    //   setFile(e.target.files[0]);
+    // };
+  
+    // const handleUpload = () => {
+    //   // قم بتنفيذ رمز الرفع هنا، يمكنك استخدام مكتبة axios أو fetch للقيام بذلك
+    //   console.log('تم رفع الصورة:', file);
+    // };
+    const onDrop = useCallback(acceptedFiles => {
+        // هنا يمكنك التعامل مع الملفات المقبولة (acceptedFiles)
+        // مثلاً، يمكنك إجراء إرسال الصورة إلى الخادم أو عرضها مباشرة في التطبيق
+        console.log(acceptedFiles);
+      }, []);
     
-    //   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-    render() { 
-        return (
-            <div>
-                       <p >تعديل المقال</p>
-                          <h5 dir="rtl" className='add2' > العنوان</h5>
-                           <input  type='text' className='txt' />
-            
-                            <h5   className='add3' >  المصدر</h5>
-                              <input   type='text' className='txt2' />
-             
-                           
-                          <textarea  className="subject" id="" cols="30" rows="10"  ></textarea>
-            
-                          <button  onClick={this.change} className='btn'> حفظ</button> 
-             
-            
-                         <button  onClick={this.handleClick} className='btn3'> الغاء الامر</button>
-                          
-                      </div>
-        );
-    }
+      const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+    
+    
+    return ( 
+        <div>
+            <h1>تعديل المقال</h1>
+            <Form dir='rtl'>
+      <Form.Group className="mb3" controlId="exampleForm.ControlInput1">
+        <Form.Label className='fl'>  العنوان</Form.Label>
+        <Form.Control  className='fc' type="text"  />
+      </Form.Group>
+      <Form.Group  className="mb4" controlId="exampleForm.ControlInput1">
+        <Form.Label className='fl' >  المصدر</Form.Label>
+        <Form.Control  className='fc' type=" text"   />
+      </Form.Group>
+      <Form>
+      {/* <Form.Group controlId="formImage">
+        <Form.File 
+          id="custom-file"
+          label="اختر صورة"
+          onChange={handleFileChange}
+          custom
+        />
+      </Form.Group> */}
+      {/* <Button variant="primary" onClick={handleUpload}>
+        رفع الصورة
+      </Button> */}
+    </Form>
+
+    <div className="fp">
+                        <label className="lable3" htmlFor="event_image_path" ></label>
+                              {/* <br></br>  <span style={{color: 'red'}}>
+                                    disabled cause of backend API handle
+                                </span> */}
+                    <div {...getRootProps()} style={ {
+                                border: 'none',
+                                borderRadius: '4px',
+                                padding: '20px',
+                                 textAlign: 'center',
+                                  cursor: 'pointer',
+                                  width:'750px',
+                                  height:'79px',
+                                  marginRight:'-220px',
+                                  backgroundColor:' rgb(218, 212, 212)'
+
+                                  }}>
+                          <input {...getInputProps()} />
+                                {isDragActive ? (
+                        <p>قم بإسقاط الملف هنا...</p>
+                                     ) : (
+                                    <p className='fl2'>       
+                                        <span className="material-icons-outlined">file_upload</span>    
+                                        رفع الصورة  
+                                        
+                                            </p>
+                         )}
+                                     </div>
+
+     
+                                          
+
+ 
+ 
+                 </div>
+       
+      <Form.Group className="mb5" controlId="exampleForm.ControlTextarea1">
+        <Form.Label className='fl'>   المقال</Form.Label>
+        <Form.Control  className='fcc' as="textarea" rows={3} />
+
+      </Form.Group>
+      <Button className="b1" variant="primary"> حفظ</Button>{' '}
+      <Button className="b2" variant="light"> الغاء الامر</Button>{' '}
+    </Form>
+        </div>
+     );
 }
  
 export default EditArticle;
-// const EditArticle = () => {
-//       handleClick =()=>{
-//     console.log(this.state.Name)
-//   }
-//     return ( 
-//         <div>
-//             <p >تعديل المقال</p>
-//                <h5 dir="rtl" className='add2' > العنوان</h5>
-//                 <input  type='text' className='txt' />
-
-//                 <h5   className='add3' >  المصدر</h5>
-//                 <input   type='text' className='txt2' />
- 
-               
-//             <textarea  className="subject" id="" cols="30" rows="10" placeholder="test text"></textarea>
-
-//             <button className='btn'>
-//                 حفظ
-//             </button> 
- 
-
-//            <button className='btn3'> الغاء الامر</button>
-//            <button onClick={this.handleClick}> Click Here </button>
-//         </div>
-//      );
-// }
- 
-// export default EditArticle;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,10 +5,11 @@ import React from "react";
 import SideBar from "../components/SideBar";
 import AddArticle2 from "../pages/AddArticle2";
 import Message from '../pages/Message';
- import EditArticle from '../pages/EditArticle'
+ 
 //import AddVideo2 from "../pages/AddVideo2";
 import AddVideo3 from "../pages/AddVideo3";
-import Edit from "../pages/Edit";
+ 
+import EditArticle from "../pages/EditArticle";
  
 //import AddVideo2 from "../pages/AddVideo2"
  
@@ -17,15 +18,14 @@ export const RouterComponent = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={< SideBar/>} />
-        {/* <Route path="/" element={< SideBar >} /> */}
-        {/* <Route path="/sidebar" element={<SideBar  />} /> */}
+        
+        <Route path="/" element={< SideBar />} />
+        <Route path="/sidebar" element={<SideBar  />} />
         <Route path="/message" element={< Message />} />
-        <Route path="/editarticle" element={<EditArticle/>} />
+         
         <Route path="/addarticle " element={<AddArticle2/>} />
-        <Route path="/edit" element={<Edit/>} />
-
-        {/* <Route path="/add" element={< AddVideo2  />} /> */}
+        <Route path="/edit" element={<EditArticle/>} />
+        <Route path="/addvideo" element={< AddVideo3  />} />
        
       </Routes>
     </Router>
