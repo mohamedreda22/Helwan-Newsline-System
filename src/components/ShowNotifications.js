8f183b93ffbabfa9450fd06adbd7ea5d7f643b57
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2024-04-29 13:03:47 +0300

    notifications window

--- src/components/ShowNotifications.js
+++ src/components/ShowNotifications.js
@@ -52,7 +52,7 @@
 // };
 
 // export default ShowNotifications;
-import React, { useState, useEffect } from 'react';
+/* import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
@@ -108,4 +108,60 @@ const ShowNotifications = () => {
   );
 };
 
+export default ShowNotifications; */
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import "../styles/notificationsWindow.css"
+
+const NotificationWindow = ({ notifications, onClick }) => (
+  <div className="notification-window">
+    <h2>Notifications</h2>
+    <ul>
+      {notifications.map(notification => (
+        <li key={notification.notification_id} onClick={() => onClick(notification.post_id)}>
+          <div className="notification-message">{notification.notification_message}</div>
+          <div className="notification-date">{notification.notification_date}</div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const ShowNotifications = () => {
+  const [notifications, setNotifications] = useState([]);
+  const sourceId = Cookies.get('source_id'); 
+
+  useEffect(() => {
+    console.log("Source ID:", sourceId);
+    if (sourceId) {
+      fetchNotifications(); 
+      const intervalId = setInterval(fetchNotifications, 60000); 
+
+      return () => clearInterval(intervalId); 
+    }
+  }, [sourceId]);
+
+  const fetchNotifications = async () => {
+    try {
+      const response = await axios.get(`http://localhost:9090/university/notifications/source/${sourceId}`);
+      console.log("Response:", response.data); 
+      setNotifications(response.data);
+    } catch (error) {
+      console.error('Error fetching notifications:', error);
+    }
+  };
+
+  const handleNotificationClick = (postId) => {
+    // Handle navigation to the corresponding post/event/article page
+    console.log(`Navigating to post/event/article with id ${postId}`);
+  };
+
+  return (
+    <NotificationWindow notifications={notifications} onClick={handleNotificationClick} />
+  );
+};
+
 export default ShowNotifications;
+
--- src/layouts/SideBar.js
+++ src/layouts/SideBar.js
@@ -1,88 +1,114 @@
-import React from 'react'; 
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import React, { useEffect, useState } from 'react'; 
+import { Sidebar, Menu, SubMenu,MenuItem} from 'react-pro-sidebar';
 import logo from "../assets/images/logo.png";
 import '../styles/SideBar.css';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import NotificationWindow from '../components/ShowNotifications';
+import axios from 'axios';
 
-export default function SideBar  ()  {
-    const handleExit=()=>{
+export default function SideBar() {
+    const [notifications, setNotifications] = useState([]);
+    const [showNotifications, setShowNotifications] = useState(false);
+
+    const handleExit = () => {
         Cookies.remove('userRole');
         window.location.href = '/';
     }
+
+    useEffect(() => {        
+        fetchNotifications(); 
+        const intervalId = setInterval(fetchNotifications, 60000); 
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const fetchNotifications = async () => {
+        try {
+          const sourceId = Cookies.get('source_id'); // Get sourceId from cookies
+          if (sourceId) {
+            const response = await axios.get(`http://localhost:9090/university/notifications/source/${sourceId}`);
+            console.log("Response:", response.data); 
+            setNotifications(response.data);
+          }
+        } catch (error) {
+          console.error('Error fetching notifications:', error);
+        }
+    };
+
+    const handleNotificationClick = (postId) => {
+        console.log(`Navigating to post/event/article with id ${postId}`);
+    };
+
     return ( 
         <div className='sideBar' dir="rtl">
             <div className="logo">
-            <img src={logo} alt="logo" style={{ width: '100px', height: 'auto' }} />
+                <img src={logo} alt="logo" style={{ width: '100px', height: 'auto' }} />
             </div>
             <div className="menuItemWrapper">
-            <Sidebar className='side2'>
-                <Menu>
-                <Nav.Link as={Link} to="/notifications" className='active'>
-                        <span className="material-icons-outlined">question_mark</span> الإشعارات
-                    </Nav.Link>
-                  <SubMenu label=" اهم الاحداث">
-                     <Nav.Link as={Link} to="/addEvent" className='active'>
-                        <span className="material-icons-outlined">add</span> اضافة حدث
-                     </Nav.Link>
-                     <Nav.Link as={Link} to="/showEvents" className='active'>
-                        <span className="material-icons-outlined">calendar_month</span> كل الاحداث
-                    </Nav.Link>
-                  </SubMenu>
-                  <SubMenu label=" الرياضة" >
-                     <Nav.Link as={Link} to="/addSport" className='active'>
-                        <span className="material-icons-outlined">add</span> اضافة رياضة
-                     </Nav.Link>
-                     <Nav.Link as={Link} to="/showSports" className='active'>
-                        <span className="material-icons-outlined">sports_football</span> كل الرياضات 
-                    </Nav.Link>
-                  </SubMenu>
-                    <SubMenu label="المقالات">
-                        <Nav.Link href="addarticle" className='active'>
-                            <span className="material-icons-outlined">add</span> اضافة مقال
+                <Sidebar className='side2'>
+                    <Menu>
+                        <Nav.Link onClick={() => setShowNotifications(!showNotifications)} className='active'>
+                            <span className="material-icons-outlined">notifications</span> الإشعارات {notifications.length > 0 && <span>({notifications.length})</span>}
                         </Nav.Link>
-                        <Nav.Link href="articles1" className='active'>
-                            <span className="material-icons-outlined">article</span> كل المقالات
+                        {showNotifications && <NotificationWindow notifications={notifications} onClick={handleNotificationClick} />}
+                        <SubMenu label=" اهم الاحداث">
+                            <Nav.Link as={Link} to="/addEvent" className='active'>
+                                <span className="material-icons-outlined">add</span> اضافة حدث
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/showEvents" className='active'>
+                                <span className="material-icons-outlined">calendar_month</span> كل الاحداث
+                            </Nav.Link>
+                        </SubMenu>
+                        <SubMenu label=" الرياضة" >
+                            <Nav.Link as={Link} to="/addSport" className='active'>
+                                <span className="material-icons-outlined">add</span> اضافة رياضة
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/showSports" className='active'>
+                                <span className="material-icons-outlined">sports_football</span> كل الرياضات 
+                            </Nav.Link>
+                        </SubMenu>
+                        <SubMenu label="المقالات">
+                            <Nav.Link href="addarticle" className='active'>
+                                <span className="material-icons-outlined">add</span> اضافة مقال
+                            </Nav.Link>
+                            <Nav.Link href="articles1" className='active'>
+                                <span className="material-icons-outlined">article</span> كل المقالات
+                            </Nav.Link>
+                        </SubMenu>
+                        <SubMenu label="كل الفيديوهات">
+                            <Nav.Link href="addVideo" className='active'>
+                                <span className="material-icons-outlined">add</span> اضافة فيديو
+                            </Nav.Link>
+                            <Nav.Link href="videoList" className='active'>
+                                <span className="material-icons-outlined">play_circle</span> كل الفيديوهات
+                            </Nav.Link>
+                        </SubMenu>
+                        <SubMenu label=" اهم الاخبار" >
+                            <Nav.Link as={Link} to="/addNews" className='active'>
+                                <span className="material-icons-outlined">add</span> اضافة خبر
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/showNews" className='active'>
+                                <span className="material-icons-outlined">newspaper</span> كل الأخبار 
+                            </Nav.Link>
+                        </SubMenu>               
+                        <SubMenu label="منشور ">
+                            <Nav.Link href="addPost" className='active'>
+                                <span className="material-icons-outlined">add</span> اضافة منشور
+                            </Nav.Link>
+                            <Nav.Link href="showPosts" className='active'>
+                                <span className="material-icons-outlined">assistant</span> كل المنشورات
+                            </Nav.Link>
+                        </SubMenu>
+                        <Nav.Link as={Link} to="/faq" className='active'>
+                            <span className="material-icons-outlined">question_mark</span> الاسئلة الشائعة
                         </Nav.Link>
-                    </SubMenu>
-                    <SubMenu label="كل الفيديوهات">
-                        <Nav.Link href="addVideo" className='active'>
-                            <span className="material-icons-outlined">add</span> اضافة فيديو
+                        {[...Array(1)].map((_, index) => <MenuItem key={index}></MenuItem>)}
+                        <Nav.Link as={Link} to="/logout" onClick={handleExit} className='active'>
+                            <span className="material-icons-outlined">logout</span> تسجيل الخروج
                         </Nav.Link>
-                        <Nav.Link href="videoList" className='active'>
-                            <span className="material-icons-outlined">play_circle</span> كل الفيديوهات
-                        </Nav.Link>
-                    </SubMenu>
-{/*                     <Nav.Link href="notifications" className='active'>
-                        <span className="material-icons-outlined">add_alert</span> الاشعارات
-                    </Nav.Link> */}
-                     <SubMenu label=" اهم الاخبار" >
-                     <Nav.Link as={Link} to="/addNews" className='active'>
-                        <span className="material-icons-outlined">add</span> اضافة خبر
-                     </Nav.Link>
-                     <Nav.Link as={Link} to="/showNews" className='active'>
-                        <span className="material-icons-outlined">newspaper</span> كل الأخبار 
-                    </Nav.Link>
-                  </SubMenu>               
-                    <SubMenu label="منشور ">
-                        <Nav.Link href="addPost" className='active'>
-                            <span className="material-icons-outlined">add</span> اضافة منشور
-                        </Nav.Link>
-                        <Nav.Link href="showPosts" className='active'>
-                            <span className="material-icons-outlined">assistant</span> كل المنشورات
-                        </Nav.Link>
-                    </SubMenu>
-                    <Nav.Link as={Link} to="/faq" className='active'>
-                        <span className="material-icons-outlined">question_mark</span> الاسئلة الشائعة
-                    </Nav.Link>
-                    {[...Array(1)].map((_, index) => <MenuItem key={index}></MenuItem>)}
-                    <Nav.Link as={Link} to="/logout" onClick={handleExit} className='active'>
-                        <span className="material-icons-outlined">logout</span> تسجيل الخروج
-                    </Nav.Link>
-
-                </Menu>
-            </Sidebar>
+                    </Menu>
+                </Sidebar>
             </div>
         </div>
     );
