b2369d38226e4326719d8055e4f5a8b5a748c7fe
Hager Amr <118662525+hager13203@users.noreply.github.com>
2024-05-04 02:41:10 +0300

    remove useless comments

---
+++ src/pages/NotificationsTrash.js
@@ -0,0 +1,39 @@
+// import React from "react";
+// import SideBar from '../layouts/SideBar';
+// import ShowNotifications from "../components/ShowNotifications";
+// import SearchBar from "../layouts/SearchBar";
+// import "../styles/Notifications.css";
+// import Button from "react-bootstrap/Button";
+// import AddNotificationForm from "../components/AddNotificationForm";
+// import { Link } from 'react-router-dom';
+
+
+// function Notifications() {
+//   return (
+//     <div className="container-fluid bg-gray">
+//       <div className="row">
+//         <div className="col-lg-10">
+//           <SearchBar />
+//           <h1 dir="rtl">الإشعارات</h1>
+//           <div dir="rtl" className="links ">
+//             <Link to="/notifications">
+//               <Button variant="link">الإشعارات</Button>
+//             </Link>{" "}
+//             <Link to="/emails">
+//               <Button variant="link">الإيميلات</Button>
+//             </Link>{" "}
+//           </div>
+//           <div className="container-2">
+//             <AddNotificationForm />
+//             <ShowNotifications />
+//           </div>
+//         </div>
+//         <div className="col-lg-2">
+//           <SideBar />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Notifications;
